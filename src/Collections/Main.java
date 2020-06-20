b227691529b1bd5fd3bc4b1f62d186d478cbbe2f
rumigor <rumigor@gmail.com>
2020-06-20 13:46:07 +0300

    Initial commit

---
+++ src/Collections/Main.java
@@ -0,0 +1,10 @@
+package Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [] words =  {"арбуз", "стол", "стул", "кот", "собака", "кот", "хвост", "собака", "кот", "миска", "корова",
+                "арбуз", "лента", "кирпич", "стол", "крыша", "кот"};
+        
+    }
+}
