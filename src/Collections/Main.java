cf6f24830ddd596058521cb04d3c674a58e1183f
rumigor <rumigor@gmail.com>
2020-06-20 16:18:46 +0300

    Collections #lesson3!

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    static Map <String, List<Integer>> records = new HashMap<>();
 
     public static void main(String[] args) {
 //        Первое задание
@@ -20,27 +19,17 @@ public class Main {
         wordsNum.forEach((k,v) ->
                 System.out.println(k + ": " + v));
 //        Второе задание
-        add(new Phonebook("Иванов", 2401523));
-        add(new Phonebook("Сидоров", 5846574));
-        add(new Phonebook("Клоков", 4556788));
-        add(new Phonebook("Иванов", 5678990));
-        add(new Phonebook("Сидоров", 7658990));
-        add(new Phonebook("Румянцев", 9997788));
-        add(new Phonebook("Иванов", 6665544));
-        get("Иванов");
-        get("Сидоров");
-        get("Румянцев");
-    }
-    public static void add(Phonebook record) {
-        List<Integer> numbers = new ArrayList<>();
-        if (records.containsKey(record.getSurname())) {
-            numbers.addAll(records.get(record.getSurname()));}
-        numbers.add(record.getNumber());
-        records.put(record.getSurname(), numbers);
-    }
-
-    public static void get(String surname) {
-        System.out.println(surname + ": " + records.get(surname));
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Иванов", 2401523);
+        phonebook.add("Сидоров", 5846574);
+        phonebook.add("Клоков", 4556788);
+        phonebook.add("Иванов", 5678990);
+        phonebook.add("Сидоров", 7658990);
+        phonebook.add("Румянцев", 9997788);
+        phonebook.add("Иванов", 6665544);
+        phonebook.get("Иванов");
+        phonebook.get("Сидоров");
+        phonebook.get("Румянцев");
     }
 
 }
